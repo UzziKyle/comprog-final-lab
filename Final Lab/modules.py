86d0559508a963d280726c2b5824d1912d0f1642
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-05-18 21:20:26 +0800

    Add files via upload

--- Final Lab/modules.py
+++ Final Lab/modules.py
@@ -10,31 +10,32 @@ class Modules():
         students_records.append(new_student)
 
     def search_student(students_records = list) -> None:
-        name_search = input("Enter the name of the student: ").strip().upper()
+        searched_name = input("Search by name: ").strip().upper()
         print('\n')
         for student in students_records:
-            if student["Full Name"] == name_search:
+            if searched_name in student["Full Name"]:
                 for info in student:
                     print("{}: {}".format(info, student[info]))
+                print("\n")
                 
     def update_course(students_records = list) -> None:
-        name_search = input("Search by full name: ").strip().upper()
+        searched_name = input("Enter student's full name: ").strip().upper()
         print('\n')
         for student in students_records:
-            if student["Full Name"] == name_search:
+            if student["Full Name"] == searched_name:
                 for info in student:
                     if (info == "Full Name") or (info == "Course"):
                         print("{}: {}".format(info, student[info]))
 
         course_update = input("\nEnter student's new program: ").strip().upper()
         for student in students_records:
-            if student["Full Name"] == name_search:
+            if student["Full Name"] == searched_name:
                 for info in student:
                     if info == "Course":
                         student[info] = course_update
 
         for student in students_records:
-            if student["Full Name"] == name_search:
+            if student["Full Name"] == searched_name:
                 for info in student:
                     print("{}: {}".format(info, student[info]))
 
@@ -44,8 +45,7 @@ class Modules():
         for student in students_records:
             if student["Course"] == course_search:
                 for info in student:
-                    print("{}: {}".format(info, student[info]))
-                print("\n")
+                    print("{}: {}\n".format(info, student[info]))
 
     def count_students(students_records = list, count = 0) -> None:
         if len(students_records) - count != 0:
