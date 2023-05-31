ee99f02b8a50164ad8456f31b1d0d5ae8a8b4e77
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-05-31 16:08:25 +0800

    Update security.py
    refactored, added error handling

--- Final Lab/security.py
+++ Final Lab/security.py
@@ -1,70 +1,87 @@
-# SECURITY FUNCTIONS AND MAIN PROGRAM CALL
-import os
+from os import system as sys
 from time import sleep as sp
 
-# A function that asks for a username. 
-def userNameAgain(n):
-    os.system('cls')
-    username = input("\nEnter username again: ").strip()
-    n
 
-    if username == 'ComputerScience':
-        getPassword(3)
+def get_credentials() -> list:
+    return ["ComputerScience", "PSU88@"]
 
-    else:
-        os.system('cls')
-        n = n - 1
-        print("\nInvalid username")
+def login() -> None:
+    credentials = get_credentials()
 
-        if n != 0:
-            print("You have (",n,") tries left")
-            sp(1)
-            userNameAgain(n)
+    for tries in range(3):
+        try:
+            sys('cls')
+            username = input("Enter username: ").strip()
+
+            if username == credentials[0]:
+                break
+
+            elif tries == range(3)[-1]:
+                sys('cls')
+                print("Limit has reached")
+                exit()
+
+            else:
+                print("Invalid username, you have {} attempt(s) left".format(range(3)[-1] - tries), end="\r")
+                sp(0.5)
+
+        except ValueError:
+            print("Invalid input", end="\r")
+            sp(0.5)
+
+    for tries in range(3):
+        try:
+            sys('cls')
+            password = input("Enter password: ").strip()
+
+            if password == credentials[1]:
+                break
+
+            elif tries == range(3)[-1]:
+                sys('cls')
+                print("Limit has reached")
+                exit()
+
+            else:
+                print("Invalid password, you have {} tries left".format(range(3)[-1] - tries), end="\r")
+                sp(0.5)
+
+        except ValueError:
+            print("Invalid input", end="\r")
+            sp(0.5)
+
+
+security_modes = {
+    1: {
+        "function": login,
+        "name": "Log In",
+    },
+    2: {
+        "function": quit,
+        "name": "Exit",
+    }
+}
+
+
+def run_security() -> None:
+    while True:
+        try:
+            sys('cls')
+            print("Security Menu:")
+
+            for mode_num in security_modes:
+                print("{} - {}".format(mode_num,security_modes[mode_num]["name"]))
+
+            user_input = int(input("Enter mode num: ").strip())
+
+        except ValueError:
+            print("Invalid Input", end="\r")
+            sp(0.5)
+
+        except KeyError:
+            print("Invalid Input", end="\r")
+            sp(0.5)
 
         else:
-            invalidUsername()
-
-
-# A function that asks for a passcode. 
-def getPassword(n):
-    os.system('cls')
-    password = input("Enter password: ").strip()
-    n
-
-    # If passcode is correct, it'll import the main program.
-    if password == "PSU88@":
-        print('\n---------------------------------------------------')
-
-    # Else, it stops the program.
-    else:
-        os.system('cls')
-        n = n - 1
-        print("\nInvalid passcode")
- 
-        if n != 0:
-            print("You have (",n,") tries left\n")
-            sp(1)
-            getPassword(n)
-
-        else:
-            invalidUsername()
-
-# Will be called if the input has reached its error limit
-def invalidUsername():
-    os.system('cls')
-    print("Sorry.")
-    sp(5)
-    os.system('cls')
-    exit()
-
-# StartUp - the start of the program, first attempt of asking the username
-def askUsername():
-    os.system('cls')
-    username = input("\nEnter username: ").strip()
-    if username == 'ComputerScience':
-        getPassword(3)
-
-    else:
-        print("\nInvalid username")
-        print("You have (",2, ") tries left")
-        userNameAgain(2)
+            security_modes[user_input]["function"]() # Runs the chosen module
+            break
